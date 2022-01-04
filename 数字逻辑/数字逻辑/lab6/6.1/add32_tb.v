98cfa33e6b73231a0d291db835638ac5beb566fa
赵方程 <equationzhao@foxmail.com>
2022-01-05 01:28:07 +0800

    update

--- 数字逻辑/数字逻辑/lab6/6.1/add32_tb.v
+++ 数字逻辑/数字逻辑/lab6/6.1/add32_tb.v
@@ -1,25 +1,27 @@
 `timescale 1ns / 1ps
 
 module add32_tb();
-    reg [31:0] ina;
-    reg [31:0] inb;
-    reg clk;
-    reg cin;
-    wire cout;
-    wire [31:0] sum;
-    initial begin
-        ina = 4'bxxxx;
-        inb = 4'bxxxx;
-        cin = 1'bx;
-        clk = 0;
-    end
-    always #100 clk = ~clk;
-    always@(posedge clk)begin
-        ina={$random}%2**30;
-        inb={$random}%2**30;
-        cin={$random}%2;
-        #150; 
-        cin=1'bx;
-    end
-   add32_4 a(ina,inb,clk,cin,cout,sum);
+	reg [31:0]a;
+	reg [31:0]b;
+	reg cin;
+	reg clk;
+	wire [31:0]s0;
+	wire cout0;
+	initial begin
+		a = 4'bxxxx;
+		b = 4'bxxxx;
+		cin = 1'bx;
+		clk = 0;
+	end
+	always #100 clk = ~clk;
+	always@(posedge clk)begin
+		a={$random}%2**30;
+		b={$random}%2**30;
+		cin={$random}%2;
+		#150;
+		cin=1'bx;
+	end
+
+	add32_2 A(a,b,clk,cin,cout0,s0);
+
 endmodule
